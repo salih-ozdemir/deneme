cd4d559a13f4d4d11297822f616b9f2404eb2926
salih-ozdemir <salihozdemir@windowslive.com>
2023-12-21 16:32:13 +0300

    Update DenemeController.java

--- src/main/java/ozdemir/salih/deneme/controller/DenemeController.java
+++ src/main/java/ozdemir/salih/deneme/controller/DenemeController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+This is Comment File Another Users...!
+*/
 @RestController
 @RequestMapping("/api")
 public class DenemeController {
